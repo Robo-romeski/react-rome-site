9dcba4025f82b89dc3753b007a2e51cebc5cd0b5
RomeMoore <jmoore@defensemobile.com>
2017-06-03 16:53:56 -0400

    background changed

---
+++ src/components/PrimaryDiv.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class primarydiv extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Primary Div</h1>
+      </div>
+    );
+  }
+}
+
+export default primarydiv;
