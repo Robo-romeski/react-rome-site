13dcddac5130e396b6d60e2878dd79d326ad9558
akmoore85 <akmoore85@gmail.com>
2017-07-21 15:08:26 -0400

    animate css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './animate.css';
 import PrimaryDiv from './components/PrimaryDiv';
 
 class App extends Component {
