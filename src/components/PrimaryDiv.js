cc838409795fc94c836e530dbbee7c5c61704165
akmoore85 <akmoore85@gmail.com>
2017-07-21 20:00:51 -0400

    from alex-devin computer

--- src/components/PrimaryDiv.js
+++ src/components/PrimaryDiv.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 class primarydiv extends Component {
   render() {
     return (
+      <div>
+      <div id="shim"></div>
 <div id="content">
-  <div id="logobox" className="logo_box animated zoomInLeft"><h1>Jerome<br/>O.<br/>Moore</h1></div>          
+  <div id="logobox" className="logo_box animated zoomInLeft"><h1>Jerome<br/>O.<br/>Moore</h1></div>
   <div className="main_box">
     <h2>Coming Soon!<br/><span>In the mean time you can connect with me with the information below...</span></h2>
-    
+
     <ul className="info">
       <li>
         <h3>P</h3>
@@ -25,12 +27,13 @@ class primarydiv extends Component {
         <h3>S</h3>
         <p className="social">
           <a href="skype:jerome_0_moore?add"><i className="fa fa-skype fa-3x tw"></i></a>
-          <a href="https://github.com/Robo-romeski" target="_blank"><i className="fa fa-github-square fa-3x tw"></i></a>  
-          <a href="https://www.linkedin.com/in/jerome-moore-31bb3620" target="_blank"><i className="fa fa-linkedin-square fa-3x tw"></i></a>    
+          <a href="https://github.com/Robo-romeski" target="_blank"><i className="fa fa-github-square fa-3x tw"></i></a>
+          <a href="https://www.linkedin.com/in/jerome-moore-31bb3620" target="_blank"><i className="fa fa-linkedin-square fa-3x tw"></i></a>
         </p>
       </li>
     </ul>
   </div>
+</div>
 </div>
     );
   }
