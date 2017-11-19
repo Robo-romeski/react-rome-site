b641ae315d12f361c14d49efa0db12159ddf0e3f
Romeski <controltower@Romeskis-MBP.fios-router.home>
2017-11-18 19:29:40 -0500

    remove parallax.js from node modules

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { TweenMax, TweenLite } from 'gsap';
-import Parallax from 'parallax.js';
 import ParallaxScene from './ParallaxScene.js';
 import ParallaxLayer from './ParallaxLayer.js';
 // import Cascade from '../Cascade.css';
