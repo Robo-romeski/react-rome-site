ec202d0540d92e22bf40111e97c798aed29e235f
Romeski <controltower@Romeskis-MBP.fios-router.home>
2017-11-18 20:04:09 -0500

    debugging css issues

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { TweenLite } from 'gsap';
 import ReactDOM from 'react-dom';
 import './components/css/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.css';
+import './components/css/home.css';
+import './components/css/animate.css';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 // import './index.css';
