fc23b49d9187c384df581ca7166d9901d588569e
Romeski <controltower@Romeskis-MBP.home>
2018-01-02 22:54:22 -0500

    second page added

--- src/components/Top.js
+++ src/components/Top.js
@@ -11,52 +11,83 @@ const Robo = {
   phone: "1(347)788-8436"
 }
 
+class Bullshit extends Component {
+
+  render(){
+    return(
+      <div>
+       <h1>BIG BULLSHIT</h1>
+      </div>
+    )
+  }
+}
+
+
 export default class Top extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      defaultScreen:true
+    }
   }
-  render() {
 
-    return (
-      <div className="contain">
-        <ParallaxScene>
-          <ParallaxLayer depth={0.4}>
-            <div id="home-page" className="container-fluid">
-              <div id="inner-container" className="container">
-                <div className="row">
-                  <div className="col-md-6 logo_box animated zoomInLeft">
-                    <h1>JEROME O. MOORE</h1>
-                    <Tooltip placement="right" className="in" id="tooltip-right">
-                      Tooltip right
-                    </Tooltip>
-                  </div>
-                  <div className="col-md-6">
-                    <h2>connect with me!</h2>
-                    <ul className="info">
-                      <li>
-                        <h3>P</h3>
-                        <p>{Robo.phone}<br /></p>
-                      </li>
-                      <li>
-                        <h3>E</h3>
-                        <p><a className="reslink" href={`mailto:${Robo.email}`}>{Robo.email}</a></p>
-                      </li>
-                      <li>
-                        <h3>S</h3>
-                        <p className="social">
-                          <a href="skype:jerome_0_moore?add"><i className="fa fa-skype fa-3x tw"></i></a>
-                          <a href="https://github.com/Robo-romeski" target="_blank"><i className="fa fa-github-square fa-3x tw"></i></a>
-                          <a href="https://www.linkedin.com/in/jerome-moore-31bb3620" target="_blank"><i className="fa fa-linkedin-square fa-3x tw"></i></a>
-                        </p>
-                      </li>
-                    </ul>
+  toggleScreen = () => {
+    console.log("FIRING", !this.state.defaultScreen)
+    this.setState({defaultScreen:!this.state.defaultScreen})
+  }
+
+  render() {
+    if(this.state.defaultScreen == true){
+      return (
+        <div className="contain">
+          <ParallaxScene>
+            <ParallaxLayer depth={0.4}>
+              <div id="home-page" className="container-fluid">
+                <div id="inner-container" className="container">
+                  <div className="row">
+                    <div className="col-md-6 logo_box animated zoomInLeft">
+                      <h1>JEROME O. MOORE</h1>
+                      <Tooltip placement="right" className="in" id="tooltip-right">
+                        Tooltip right
+                      </Tooltip>
+                    </div>
+                    <div className="col-md-6">
+                      <h2>connect with me!</h2>
+                      <ul className="info">
+                        <li>
+                          <h3>P</h3>
+                          <p><a className="reslink" href={`tel:${Robo.phone}`}>{Robo.phone}</a></p>
+                        </li>
+                        <li>
+                          <h3>E</h3>
+                          <p><a className="reslink" href={`mailto:${Robo.email}`}>{Robo.email}</a></p>
+                        </li>
+                        <li>
+                          <h3>W</h3>
+                          <p className="reslink finga" onClick={this.toggleScreen}>My Works</p>
+                        </li>
+                        <li>
+                          <h3>S</h3>
+                          <p className="social">
+                            <a href="skype:jerome_0_moore?add"><i className="fa fa-skype fa-3x tw"></i></a>
+                            <a href="https://github.com/Robo-romeski" target="_blank"><i className="fa fa-github-square fa-3x tw"></i></a>
+                            <a href="https://www.linkedin.com/in/jerome-moore-31bb3620" target="_blank"><i className="fa fa-linkedin-square fa-3x tw"></i></a>
+                          </p>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </ParallaxLayer>
-        </ParallaxScene>
-      </div>
-    );
+            </ParallaxLayer>
+          </ParallaxScene>
+        </div>
+      );
+    } else {
+      return(
+      <Bullshit />
+      )
+    }
+
   }
 }
