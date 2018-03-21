3f4ed21decabcd3b99675826fb32d13c019d20a5
Romeski <controltower@Romeskis-MacBook-Pro.local>
2018-03-20 21:20:41 -0400

    footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default class Footer extends Component{
     render(){
         return(
-            <button className="footer"><Link to="/"><h1>Contact</h1></Link></button>
+            <div className="footer"><Link to="/"><h1>Contact</h1></Link></div>
         )
     }
 }
